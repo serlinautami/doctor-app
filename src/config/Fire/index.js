8c9a1654aa167aba203248974ff075ac25e9f853
serlina_utami <serlinautami93@gmail.com>
2020-11-24 21:03:52 +0700

    upload project

---
+++ src/config/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAM7aifeThs7ynf1Z_1R85ThfSzxHWzZGg",
+  authDomain: "consultation-ecc68.firebaseapp.com",
+  databaseURL: "https://consultation-ecc68.firebaseio.com",
+  projectId: "consultation-ecc68",
+  storageBucket: "consultation-ecc68.appspot.com",
+  messagingSenderId: "287858581847",
+  appId: "1:287858581847:web:3af7c569e059bfff085e12",
+});
+
+const Fire = firebase;
+
+export default Fire;
